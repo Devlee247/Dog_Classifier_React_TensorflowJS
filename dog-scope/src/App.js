1c444187d5b5d52c8427bc5bf861233eb0e809da
2014031930 <inseo8070@naver.com>
2021-05-22 23:46:30 +0900

    add stateMachine and reducer

--- dog-scope/src/App.js
+++ dog-scope/src/App.js
@@ -1,5 +1,20 @@
 import './App.css';
 
+const stateMachine = {
+  initial: "initial",
+  states: {
+  initial: { on: { next: "loadingModel", text: 'Load Model' } },
+  loadingModel: { on: { next: "modelReady", text: 'Loading Model' } },
+  modelReady: { on: { next: "imageReady", text: 'Upload Image' } },
+  imageReady: { on: { next: "identifying" }, text: 'Identify Breed', showImage: true },
+  identifying: { on: { next: "complete", text: 'Identifying…'} },
+  complete: { on: { next: "modelReady" }, text: 'Reset', showImage: true, showResults: true }
+  }
+ };
+
+ const reducer = (currentState, event) =>
+ stateMachine.states[currentState].on[event] || stateMachine.initial;
+
 function App() {
   return (
     <div></div>
